575a56644af1f5f2b9938c3903066cd138ba753c
Dmitriy <112623422+CyM4aTblu@users.noreply.github.com>
2022-09-26 19:59:29 +0300

    Add files via upload

---
+++ SIAOD_1_2/SIAOD_1_2/SIAOD_1_2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int killbits(int i)
+{
+	i = i & 0xAAAAAAAA;
+	return i;
+}
+int main()
+{
+	cout << "Pease, enter yout num: " << endl;
+	int n;
+	cin >> n;
+	cout << "After set odd bits in 0: " << killbits(n);
+}
